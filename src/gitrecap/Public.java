963cbf20aebe94ebc1a922d3a5c73c4f60fa4c7d
panthersClass <neela22noor@gmail.com>
2023-01-03 23:10:46 -0500

    merge

--- src/gitrecap/Public.java
+++ src/gitrecap/Public.java
@@ -3,6 +3,7 @@ package gitrecap;
 public class Public {
 	public static void main(String[] args) {
 		System.out.println("this is insid eclipBranch");
+		System.out.println("this is for merging ");
 	}
 	
 
